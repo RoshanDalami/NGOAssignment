e93fa292d2627bd10449c299a7132db66f0f5c1c
rsunar <tech.channel.002@gmail.com>
2023-03-18 17:49:04 +0530

    fixing responsiveness

--- src/Components/President.jsx
+++ src/Components/President.jsx
@@ -3,11 +3,7 @@ import { FaPhoneAlt } from "react-icons/fa";
 import classes from './President.module.css'
 import PresidentImage from '../assets/president.jpg'
 function President() {
-    const detail = {
-        name : 'NARESH RIJAL',
-        desc : "Lorem ipsum dolor. ",
-        phone : '+977 9804470949'
-    }
+   
   return (
     <>
     <div className={classes.container}>
