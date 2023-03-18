1968608b42cf047b9a522f1447b73da7a3ad0fbc
rsunar <tech.channel.002@gmail.com>
2023-03-18 08:47:06 +0530

    fixing

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,11 +3,12 @@ import classes from './Banner.module.css'
 
 import BannerCard from '../UI/BannerCard'
 import BannerData from '../Data/BannerData'
+import uuid from 'react-uuid'
 
 
 function Banner() {
     const bannerItems = BannerData.map((data)=>{
-        return <BannerCard image={data.image} title={data.title}/>
+        return <BannerCard key={uuid()} image={data.image} title={data.title}/>
     })
   return (
     <>
