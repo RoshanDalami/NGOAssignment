a87ee25b00558a83625c74cff259c27d0bf2878b
RoshanDalami <roshandalami0@gmail.com>
2023-09-28 22:12:05 +0545

    removed members and added members

--- src/Data/TeamData.js
+++ src/Data/TeamData.js
@@ -1,17 +1,18 @@
 import President from '../assets/president.jpg'
 import Image1 from '../assets/TeamImage1.jpeg'
-import Image2 from '../assets/TeamImage2.jpeg'
+
 import Image3 from '../assets/TeamImage3.jpeg'
 import Image4 from '../assets/TeamImage4.jpg'
 import Image5 from '../assets/TeamImage5.jpeg'
 import Image6 from '../assets/TeamImage6.jpeg'
 import Image7 from '../assets/TeamImage7.jpg'
-import Image8 from '../assets/TeamImage8.jpeg'
+
 import Image9 from '../assets/TeamImage9.jpeg'
 import Image11 from '../assets/TeamImage11.jpg'
 import Image12 from '../assets/TeamImage12.jpeg'
 import Image15 from '../assets/TeamImage15.jpeg'
-import Image16 from '../assets/TeamImage16.jpeg'
+import Image16 from '../assets/TeamImage17.jpeg'
+
 
 
 const TeamData = [
@@ -34,15 +35,15 @@ const TeamData = [
 
     },
     {
-        name : "Mr. Jisnu prasad paudel" ,
-        position: 'Secretary' ,
-        image : Image16,
-
-    },
-    {
         name : "Mr.Sanu kaji Thapa" ,
         position: 'Advisor' ,
         image : Image5,
+
+    },
+    {
+        name : "Mr. Naresh Chandra Lawati" ,
+        position: 'central committee member' ,
+        image : Image16,
 
     },
     {
@@ -57,20 +58,6 @@ const TeamData = [
         image : Image12,
 
     },
-    {
-        name : "Mr. Dhinduk Bhote " ,
-        position: 'International Relation Representative' ,
-        image : Image2,
-
-    },
-    {
-        name : "Mr. Sonam Tenzin" ,
-        position: 'Youth coordinator' ,
-        image : Image8,
-
-    },
-    
-   
     {
         name : "Hon'ble Hira Gurung" ,
         position: 'Member' ,
